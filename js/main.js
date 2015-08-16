f5891edbc6cd3901c0fcf88ab4bf9ddd32c47114
Franklin Javier <franklinjalves@gmail.com>
2015-08-16 19:19:51 -0300

    Add shortcut to toggle preview and editor mode. Add stats count

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var marked = require('./marked');
 var editor = ace.edit('editor');
 var session = editor.getSession();
 var mkMode = ace.require("ace/mode/markdown").Mode;
+require('./jquery.hotkeys.js');
 
 //editor.setTheme("ace/theme/franklin");
 editor.renderer.setShowGutter(false);
@@ -30,10 +31,54 @@ marked.setOptions({
   smartypants: true
 });
 
-editor.on('input', function() {
+
+function change(e) {
+  if (editor.session.getUndoManager().isClean()) {
+    //$('#save').removeClass('disabled');
+  } else {
+    //$('#save').addClass('disabled');
+  }
+
+  var val = session.getValue();
+  var character = val.length;
+  // Remove first, numbers, trim string and match words
+  var words = val.replace(/\d+/g, '').match(/\S+/g).length;
+
+  console.log(val.replace(/\dD+/g, '').match(/\S+/g));
+
+  $('#stats').find('.character').text(character > 1 ? character + ' characters -' : character + ' character -');
+  $('#stats').find('.words').text(words > 1 ? words + ' words ' : words + ' word');
+  preview();
+}
+
+function preview() {
   document.querySelector('#preview').innerHTML = marked(session.getValue());
-});
+}
+
+//$('#save').on('click', function() {
+    //editor.session.getUndoManager().markClean()
+//});
+
+editor.on('input', change);
 
 hljs.initHighlightingOnLoad();
+
+preview();
+
+$(document, editor.container).bind('keydown', 'ctrl+p', toggle);
+editor.commands.addCommand({
+  name: "toggle-preview",
+  bindKey: {win: "Ctrl-p", mac: "Ctrl-P"},
+  exec: function(editor) {
+    toggle();
+  }
+});
+
+function toggle() {
+  $('#editor').toggle();
+  $('#preview').toggle();
+  editor.focus();
+}
+
 
 // http://jsfiddle.net/yyx990803/wy2qf6yx/light/
